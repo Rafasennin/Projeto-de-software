041b66251e65a584fc8fd3c363958a85fc6b3764
Rafael Dos Santos <rafasennin@hotmail.com>
2024-03-25 07:58:25 -0300

    update

--- src/components/HeaderComponentText.vue
+++ src/components/HeaderComponentText.vue
@@ -1,26 +1,25 @@
 <template>
-    <v-container>
-      <v-banner  color="pink-darken-1" icon="mdi-information-outline" lines="two">
-        <h3 class="text-black text-body-1 ">
-          "Cortes e Navalhas" proporciona cortes e barbas excepcionais,
-          unindo tradição artesanal e modernidade, com barbeiros experientes
-          que garantem uma experiência personalizada e revitalizante para os clientes.
-        </h3>
-      </v-banner>
-    </v-container>
-  </template>
-  
-  <script>
-  
-  export default {
-    name: 'HeaderComponentText',
-    components: {
-  
-    },
-    data: () => ({
-  
-  
-    }),
-  }
-  </script>
-  
+  <v-container>
+    <v-banner class="d-none d-sm-inline-block">
+      <h3 class="text-black text-body-1 d-none d-sm-inline-block">
+        "Cortes e Navalhas" proporciona cortes e barbas excepcionais,
+        unindo tradição artesanal e modernidade, com barbeiros experientes
+        que garantem uma experiência personalizada e revitalizante para os clientes.
+      </h3>
+    </v-banner>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderComponentText',
+  components: {
+
+  },
+  data: () => ({
+
+
+  }),
+}
+</script>
